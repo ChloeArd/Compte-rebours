a31ebde5e414bb5e324d8336a68f87fcc28dccd2
ChloeArd <chloe.ardoise@gmail.com>
2020-12-30 16:23:38 +0100

    Compte à rebours

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //Le compte a rebours doit être un objet instanciable
 
+let hours = document.getElementById("heures");
+let minutes = document.getElementById("minutes");
+let seconds =  document.getElementById("secondes");
 
 //Permettre à l'utilisateur de spécifier un temps personnalisé
 //incrémenter de 1 a chaque fois que je clique sur le bouton "hautHeures" !
@@ -7,22 +10,18 @@ let ajoutHeures = 0;
 document.getElementById("hautHeures").addEventListener("click", function (){
     setTimeout(function () {
         if (ajoutHeures < 9){
-            let spanTime = document.getElementById("heures");
-            spanTime.innerHTML = "0" + (ajoutHeures += 1);
+            hours.innerHTML = "0" + (ajoutHeures += 1);
         }
 
         else if (10 >= ajoutHeures <= 100){
-            let spanTime = document.getElementById("heures");
-            spanTime.innerHTML = (ajoutHeures += 1);
+            hours.innerHTML = (ajoutHeures += 1);
         }
 
         if (ajoutHeures === 100){
             ajoutHeures = 0;
-            let spanTime = document.getElementById("heures");
-            spanTime.innerHTML = "00";
+            hours.innerHTML = "00";
         }
     }, 500);
-    //valeursHauteTime(100, "heures");
 });
 
 let ajoutMinutes = 0;
@@ -30,22 +29,18 @@ let ajoutMinutes = 0;
 document.getElementById("hautMinutes").addEventListener("click", function (){
     setTimeout(function () {
         if (ajoutMinutes < 9){
-            let spanTime = document.getElementById("minutes");
-            spanTime.innerHTML = "0" + (ajoutMinutes += 1);
+            minutes.innerHTML = "0" + (ajoutMinutes += 1);
         }
 
         else if (10 >= ajoutMinutes <= 60){
-            let spanTime = document.getElementById("minutes");
-            spanTime.innerHTML = (ajoutMinutes += 1);
+            minutes.innerHTML = (ajoutMinutes += 1);
         }
 
         if (ajoutMinutes === 60){
             ajoutMinutes = 0;
-            let spanTime = document.getElementById("minutes");
-            spanTime.innerHTML = "00";
+            minutes.innerHTML = "00";
         }
     }, 500);
-    //valeursHauteTime(60, "minutes");
 });
 
 let ajoutSeconde = 0;
@@ -53,41 +48,34 @@ let ajoutSeconde = 0;
 document.getElementById("hautSecondes").addEventListener("click", function (){
     setTimeout(function () {
         if (ajoutSeconde < 9){
-            let spanTime = document.getElementById("secondes");
-            spanTime.innerHTML = "0" + (ajoutSeconde += 1);
+            seconds.innerHTML = "0" + (ajoutSeconde += 1);
         }
 
         else if (10 >= ajoutSeconde <= 60){
-            let spanTime = document.getElementById("secondes");
-            spanTime.innerHTML = (ajoutSeconde += 1);
+            seconds.innerHTML = (ajoutSeconde += 1);
         }
 
         if (ajoutSeconde === 60){
             ajoutSeconde = 0;
-            let spanTime = document.getElementById("secondes");
-            spanTime.innerHTML = "00";
+            seconds.innerHTML = "00";
         }
     }, 500);
-    //valeursHauteTime(60, "secondes");
 });
 
 //Décrémenter de 1 a chaque fois que je clique sur le bouton "basHeures" !
 document.getElementById("basHeures").addEventListener("click", function (){
     setTimeout(function () {
         if (ajoutHeures < 11){
-            let spanTime = document.getElementById("heures");
-            spanTime.innerHTML = "0" + (ajoutHeures -= 1);
+            hours.innerHTML = "0" + (ajoutHeures -= 1);
         }
 
         else if (10 >= ajoutHeures <= 100){
-            let spanTime = document.getElementById("heures");
-            spanTime.innerHTML = (ajoutHeures -= 1);
+            hours.innerHTML = (ajoutHeures -= 1);
         }
 
         if (ajoutHeures === -1){
             ajoutHeures = 99;
-            let spanTime = document.getElementById("heures");
-            spanTime.innerHTML = "99";
+            hours.innerHTML = "99";
         }
     }, 500);
 });
@@ -96,19 +84,16 @@ document.getElementById("basHeures").addEventListener("click", function (){
 document.getElementById("basMinutes").addEventListener("click", function (){
     setTimeout(function () {
         if (ajoutMinutes < 11){
-            let spanTime = document.getElementById("minutes");
-            spanTime.innerHTML = "0" + (ajoutMinutes -= 1);
+            minutes.innerHTML = "0" + (ajoutMinutes -= 1);
         }
 
         else if (10 >= ajoutMinutes <= 60){
-            let spanTime = document.getElementById("minutes");
-            spanTime.innerHTML = (ajoutMinutes -= 1);
+            minutes.innerHTML = (ajoutMinutes -= 1);
         }
 
         if (ajoutMinutes === -1){
             ajoutMinutes = 59;
-            let spanTime = document.getElementById("minutes");
-            spanTime.innerHTML = "59";
+            minutes.innerHTML = "59";
         }
     }, 500);
 });
@@ -117,30 +102,21 @@ document.getElementById("basMinutes").addEventListener("click", function (){
 document.getElementById("basSecondes").addEventListener("click", function (){
     setTimeout(function () {
         if (ajoutSeconde < 11){
-            let spanTime = document.getElementById("secondes");
-            spanTime.innerHTML = "0" + (ajoutSeconde -= 1);
+            seconds.innerHTML = "0" + (ajoutSeconde -= 1);
         }
 
         else if (10 >= ajoutSeconde <= 60){
-            let spanTime = document.getElementById("secondes");
-            spanTime.innerHTML = (ajoutSeconde -= 1);
+            seconds.innerHTML = (ajoutSeconde -= 1);
         }
 
         if (ajoutSeconde === -1){
             ajoutSeconde = 59;
-            let spanTime = document.getElementById("secondes");
-            spanTime.innerHTML = "59";
+            seconds.innerHTML = "59";
         }
     }, 500);
-    //valeursHauteTime(60, "secondes");
 });
 
-let hours = document.getElementById("heures");
-let minutes = document.getElementById("minutes");
-let seconds =  document.getElementById("secondes");
-
 let timeoutID;
-console.log(ajoutMinutes);
 
 //Compte à rebours
 let countUp = function() {
@@ -195,7 +171,6 @@ document.getElementById("demarrer").addEventListener("click",function (){
     document.getElementById("pause").style.display = "block";
     document.getElementById("continue").style.display = "none";
     document.getElementById("demarrer").style.display = "none";
-
     countUp();
 });
 
@@ -210,9 +185,7 @@ document.getElementById("reset").addEventListener("click", function (){
     minutes.innerHTML = "00";
     seconds.innerHTML = "00";
 
-    decompteSecondes = document.getElementById("secondes").innerHTML;
     clearTimeout(timeoutID);
-
 })
 
 let stopCountUp = function() {
